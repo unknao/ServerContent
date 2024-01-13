ad783a6c5a35edab4744e532613d00bdfa0ef7b5
unknao <paujurc@gmail.com>
2024-01-13 21:40:47 +0200

    Apparently the server is not always willing to give its ip

--- lua/microscoreboard/server/country_flags.lua
+++ lua/microscoreboard/server/country_flags.lua
@@ -1,14 +1,16 @@
 local tag = "Micro_Scoreboard_Flags"
-local ServerIP = string.gsub(game.GetIPAddress(),":..+","")
 
 hook.Add("PlayerInitialSpawn", tag, function(ply)
 	if ply:IsBot() then return end
 
 	local ip = string.gsub(ply:IPAddress(), ":..+", "")
-	if string.find(ip, "192.168.0") then ip = ServerIP end --If the ip is local, use the servers ip
+	if string.find(ip, "192.168.0") then --If the ip is local, use the servers ip
+		ip = string.gsub(game.GetIPAddress(),":..+","")
+	end
 
 	http.Fetch("http://ip-api.com/json/" .. ip,function(str)
 		local tbl = util.JSONToTable(str)
+		PrintTable(tbl)
 		ply:SetNWString("country_code", tbl.countryCode)
 		ply:SetNWString("country",tbl.country)
 	end)
