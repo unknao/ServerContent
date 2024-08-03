4365092316b8708c437da87b26867e62865d046f
unknao <paujurc@gmail.com>
2024-08-03 12:35:45 +0300

    improve nametags

--- lua/autorun/nametags.lua
+++ lua/autorun/nametags.lua
@@ -2,10 +2,12 @@ if SERVER then
 	resource.AddSingleFile("resource/fonts/Mieghommel.ttf") --Font obtained from https://online-fonts.com
 	return
 end
+if not CLIENT then return end
+
 require("svg")
 
 local tag = "nametags"
-NAMETAG_HIDE = CreateConVar("nametags_hide", "0", {FCVAR_ARCHIVE}, "Hide custom nametags.")
+local nametags_hide = CreateConVar("nametags_hide", "0", {FCVAR_ARCHIVE}, "Hide custom nametags.")
 
 surface.CreateFont(tag, {
 	font = "Mieghommel",
@@ -24,36 +26,33 @@ local SVGS = {
 
 
 --dont forget to make it readable next time
-hook.Add("PreDrawTranslucentRenderables",tag,function()
-	if NAMETAG_HIDE:GetBool() then return end
+hook.Add("PostPlayerDraw", tag, function(ply)
+	if nametags_hide:GetBool() then return end
+	if ply == LocalPlayer() and not ply:ShouldDrawLocalPlayer() then return end
 
-	for _, ply in ipairs(player.GetAll()) do
-		if ply:IsDormant() then continue end
-		if ply == LocalPlayer() and not ply:ShouldDrawLocalPlayer() then continue end
+	local ply_or_ragdoll = IsValid(ply:GetRagdollEntity()) and ply:GetRagdollEntity() or ply
+	local head_attach = ply_or_ragdoll:LookupBone("ValveBiped.Bip01_Head1")
+	if not head_attach then return end
 
-		local ply_or_ragdoll = IsValid(ply:GetRagdollEntity()) and ply:GetRagdollEntity() or ply
-		local head_attach = ply_or_ragdoll:LookupBone("ValveBiped.Bip01_Head1")
-		if not head_attach then continue end
+	local head,_ = ply_or_ragdoll:GetBonePosition(head_attach)
 
-		local head,_ = ply_or_ragdoll:GetBonePosition(head_attach)
+	if head == ply_or_ragdoll:GetPos() then
+		head = ply_or_ragdoll:GetBoneMatrix(head_attach)
+	end
 
-		if head == ply_or_ragdoll:GetPos() then
-			head = ply_or_ragdoll:GetBoneMatrix(head_attach)
-		end
+	local scale = ply:GetModelScale() --take size into account
+	local head_up = ply_or_ragdoll:GetUp()
+	local pos = head + head_up * 20 * scale
+	local dist = EyePos():Distance(pos)
+	if dist >= 400 * scale then return end
 
-		local scale = ply:GetModelScale() --take size into account
-		local head_up = ply_or_ragdoll:GetUp()
-		local pos = head + head_up * 20 * scale
-		local dist = EyePos():Distance(pos)
-		if dist >= 400 * scale then continue end
+	local ang = EyeAngles()
 
-		local ang = EyeAngles()
+	--necessary evil?
+	ang:RotateAroundAxis( ang:Up(), -90 )
+	ang:RotateAroundAxis( ang:Forward(), 90 )
 
-		--necessary evil?
-		ang:RotateAroundAxis( ang:Up(), -90 )
-		ang:RotateAroundAxis( ang:Forward(), 90 )
-
-		cam.Start3D2D(pos,ang, 0.03 * scale)
+	cam.Start3D2D(pos,ang, 0.03 * scale)
 		local text = ply:Name()
 
 		local themnorm = (pos - EyePos()):GetNormalized()
@@ -82,7 +81,6 @@ hook.Add("PreDrawTranslucentRenderables",tag,function()
 			svg.Draw(SVGS[rank], rankpadding + 8, 8, ply.Nametags.cShadow)
 			svg.Draw(SVGS[rank], rankpadding, 0, ply.Nametags.cName)
 		end
-		cam.End3D2D()
-	end
+	cam.End3D2D()
 end)
 
