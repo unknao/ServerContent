d222112cbe465c2e4a9f428acb0bc93a07e0ab20
unknao <paujurc@gmail.com>
2025-05-18 19:09:03 +0300

    removed catastrophic code

--- lua/autorun/weaponstab.lua
+++
@@ -1,8 +0,0 @@
-local Whitelist = {
-    ["Default"] = true,
-    ["Wiremod"] = true,
-    ["Other"] = true
-}
-hook.Add("PreRegisterSWEP", "weaponstab", function(swep)
-    if not Whitelist[swep.Category] then return false end
-end)
