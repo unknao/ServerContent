8b980fdd6a3d8f0ee1f571d570380d58bb823879
unknao <paujurc@gmail.com>
2025-06-26 00:57:16 +0300

    token sounds better

--- lua/autorun/client/autoretry.lua
+++ lua/autorun/client/autoretry.lua
@@ -1,16 +1,16 @@
 local tag = "timeout_autoretry"
 local url = GetConVar("sv_downloadurl"):GetString()
-local server_instance
+local token
 
-http.Fetch(url .. "/serverinstance.txt", function(str)
-    server_instance = str
+http.Fetch(url .. "/token.txt", function(str)
+    token = str
 end)
 
 timer.Create(tag, 3, 0, function()
     if not GetTimeoutInfo() then return end
 
-    http.Fetch(url .. "/serverinstance.txt", function(str)
-        if str == server_instance then return end
+    http.Fetch(url .. "/token.txt", function(str)
+        if str == token then return end
 
         timer.Remove(tag)
         print("Server is restarting, rejoining shortly.")
