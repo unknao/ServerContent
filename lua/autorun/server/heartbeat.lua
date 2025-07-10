287893832eddd2d29c8b2267bb5ece000bfe57d7
unknao <paujurc@gmail.com>
2025-07-10 13:30:22 +0300

    sv_hibernate_think 0 way of checking heartbeat

--- lua/autorun/server/heartbeat.lua
+++ lua/autorun/server/heartbeat.lua
@@ -1,3 +1,7 @@
-timer.Create("serverhealth_heartbeat", 5, 0, function()
+hook.Add("ShutDown", "auto_restart_prevent", function() --Let the srcds watchdog know that we're restarting, not crashing
+    file.Write("sv_changedlevel.txt", 1)
+end)
+
+hook.Add("GetGameDescription", "sv_heartbeat", function() --Way to know when the server is still alive with hibernating on
     file.Write("sv_heartbeat.txt", "1")
 end)
