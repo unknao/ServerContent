2b66cc4e3392fb54e7092765f8e868786c5dc4b2
unknao <paujurc@gmail.com>
2025-07-12 16:34:01 +0300

    more intricate way to detect local network clients

--- lua/microscoreboard/server/country_flags.lua
+++ lua/microscoreboard/server/country_flags.lua
@@ -1,12 +1,40 @@
-local tag = "MICRO_SCOREBOARDboard_Flags"
+local tag = "MICRO_SCOREBOARD_Flags"
+-- 10.0.0.0/8
+-- 172.16.0.0/12
+-- 192.168.0.0/16
+local net_patterns = {
+	"^10",
+	"^172.16",
+	"^192.168"
+}
+
+local function IsClientOnLocalNetwork(ply)
+	local ip = string.gsub(ply:IPAddress(), ":.+", "")
+	if ip == "loopback" or ip == "127.0.0.1" then
+		return true
+	elseif ip == game.GetIPAddress() then
+		return true
+	else
+		for i, v in ipairs(net_patterns) do
+			if not string.match(ip, v) then continue end
+
+			return true
+		end
+	end
+
+	return false
+end
 
 hook.Add("PlayerInitialSpawn", tag, function(ply)
 	if ply:IsBot() then return end
 
-	local ip = string.gsub(ply:IPAddress(), ":.+", "")
-	if string.find(ip, "192.168.0") then --If the ip is local, use the servers ip
-		ip = string.gsub(game.GetIPAddress(),":.+","")
+	local ip
+	if IsClientOnLocalNetwork(ply) then
+		ip = game.GetIPAddress()
+	else
+		ip = ply:IPAddress()
 	end
+	ip = string.gsub(ip, ":.+", "")
 
 	http.Fetch("http://ip-api.com/json/" .. ip,function(str)
 		local tbl = util.JSONToTable(str)
